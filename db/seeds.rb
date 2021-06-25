87cea17c06575a16e058fc4b989fc952fcfe1383
Daniel Evani <danielevani97@gmail.com>
2021-06-25 16:36:39 -0300

    Application controller adjusted to devise accept name and kind fields, seed tocreate users added in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: 'Daniel Admin', kind: 'admin', email: 'admin@teste.com', password: 123456)
+User.create(name: 'Alícia Writter', kind: 'writter', email: 'writter@teste.com', password: 123456)
