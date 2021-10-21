5d445b8d743751db1045cf8976ac7f85b42d12d1
Daniel Evani <danielevani97@gmail.com>
2021-10-21 13:52:38 -0300

    Callbacks to get_matchs and get_notices in home#index.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,2 @@
 module ApplicationHelper
-  require 'rest-client'
-  require 'json'
-  def get_matchs
-    begin
-      api_football_url = Rails.application.credentials[Rails.env.to_sym][:api_football_url]
-      api_football_token = Rails.application.credentials[Rails.env.to_sym][:api_football_token]
-      week_ago = (Time.now - 7.days).strftime('%y-%m-%d')
-      week_ahead = (Time.now + 7.days).strftime('%y-%m-%d')
-      url = "#{api_football_url}?action=get_events&APIkey=#{api_football_token}&timezone=America/Sao_Paulo&from=#{week_ago}&to=#{week_ahead}&team_id=4292"
-      res = RestClient.get(url)
-      matchs = JSON.parse(res.body)
-    rescue RestClient::ExceptionWithResponse => e
-      e.response
-    end
-  end
-
-  def get_notices
-    notices = Notice.all
-    range = notices.length - 3
-    notices_show = notices[range..notices.length].reverse
-  end
 end
